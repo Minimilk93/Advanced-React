2b92da2a039d8bc27948b889eae1b5927c80afba
Shelby Stanton <shelby.stanton@morrisonsplc.co.uk>
2019-08-30 16:50:34 +0100

    Adding initial files

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,40 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // Check logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
 
-module.exports = mutations;
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // check if they own that item
+
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
+};
+
+module.exports = Mutations;
---
+++ sick-fits/frontend/components/Item.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
+
+class Item extends Component {
+  render() {
+    const { item } = this.props;
+    return (
+      <ItemStyles>
+        <img src="https://source.unsplash.com/random" alt={item.title} />
+        <Title>
+          <Link
+            href={{
+              pathname: './item',
+              query: { id: item.id }
+            }}
+          >
+            {item.title}
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
+        <p>{item.description}</p>
+        <div className="buttonList">
+          <Link
+            href={{
+              pathname: 'update',
+              query: { id: item.id }
+            }}
+          >
+            <a>Edit ✏️</a>
+          </Link>
+          <button>Add to Cart</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
+        </div>
+      </ItemStyles>
+    );
+  }
+}
+
+Item.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string,
+    price: PropTypes.number
+  })
+};
+
+export default Item;
